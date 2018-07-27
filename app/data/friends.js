367fa08c2969e13b60b42eec1525f1a3098561a6
Matt <makeshifthidalgo@hotmail.com>
2018-07-27 06:47:28 -0700

    basic html done, modal connected to submit button, express/body-parser installed, dummy data is entered

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiQlIbNq7_cAhWT7Z8KHRDOAmwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fpeople%2F&psig=AOvVaw0f_oXnV8bdhnScXQdehqDf&ust=1532782951216458",
+        scores: [5, 1, 3, 2, 4, 1, 4, 5, 4, 1]
+
+    },
+    {
+        name: "Gandalf",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi7ws6JrL_cAhXov1QKHTCEAbQQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGandalf&psig=AOvVaw17FAhH5K6op84nTm_tPH-3&ust=1532783072935134",
+        scores: [2, 5, 1, 5, 2, 5, 2, 1, 2, 1]
+    },
+    {
+        name: "Neo",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj4id64rL_cAhVdCTQIHXQoB8oQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FNeo_(The_Matrix)&psig=AOvVaw0hlWaWd7LJOt_4ECNU0xhb&ust=1532783174800852",
+        score: [3, 3, 2, 5, 5, 5, 1, 4, 1, 2]
+    }
+];
+
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var PORT = process.env.PORT || 8080;
+ 
+var app = express()
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
